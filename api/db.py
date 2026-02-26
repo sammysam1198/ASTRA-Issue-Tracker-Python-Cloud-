380086a0f0c8f774249b80c91601416546a2714c
SammI Fishbein <32447824+sammysam1198@users.noreply.github.com>
2026-02-26 04:01:56 -0500

    Add database connection function in db.py

--- api/db.py
+++ api/db.py
@@ -1,1 +1,10 @@
-# db.py
+# api/db.py
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from flask import current_app
+
+def get_db_conn():
+    url = current_app.config.get("DATABASE_URL")
+    if not url:
+        raise RuntimeError("DATABASE_URL is not set")
+    return psycopg2.connect(url, cursor_factory=RealDictCursor)
